563aed24169af3157df963faf56879a14493dc4c
Eli Craig <elidcraig@gmail.com>
2022-07-01 08:37:51 -0700

    validations added to models

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,9 @@
 class Student < ApplicationRecord
+  validates :name, presence: true
+  validates :age, numericality: { 
+    only_integer: true, 
+    greater_than_or_equal_to: 18 
+  }
+  
   belongs_to :instructor
 end
