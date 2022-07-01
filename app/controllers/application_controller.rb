53fd51ceee190db182be8a6fa1947d91c83e021b
Eli Craig <elidcraig@gmail.com>
2022-07-01 08:53:12 -0700

    error messages, instructors controller actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordInvalid, with: :handle_invalid
+  rescue_from ActiveRecord::RecordNotFound, with: :handle_not_found
+
+  def handle_invalid e
+    render json: { errors: e.message }, status: 422
+  end
+
+  def handle_not_found e
+    render json: { errors: e.message }, status: 404
+  end
 end
--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -1,2 +1,25 @@
 class InstructorsController < ApplicationController
+  def index
+    render json: Instructor.all
+  end
+
+  def show
+    render json: Instructor.find(params[:id])
+  end
+
+  def create
+    new_instructor = Instructor.create!(name: params[:name])
+    render json: new_instructor, status: :created
+  end
+
+  def update
+    instructor = Instructor.find(params[:id])
+    instructor.update!(name: params[:name])
+    render json: instructor
+  end
+
+  def destroy
+    Instructor.find(params[:id]).destroy
+    head :no_content
+  end
 end
